c44f4ccd29bfff27c6b501e235b215d7e3d979ec
Senni Tan <sennitan@geotab.com>
2022-02-26 01:00:44 -0500

    finish post list

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -8,22 +8,38 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useState } from 'react';
 
-const Post = () => {
+const Post = ({user}) => {
     const userProfile = "https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg"
 
-    const user = {
-        "image-url":"https://cdn.britannica.com/26/95926-050-0228E1A6/London-Eye-River-Thames.jpg",
-        "userName":"Amy",
-        "description":"London Eye is one of most popular tourist attractions in London!",
-        "label":["lake","sky wheel"],
-        "type":"Landmark",
-        "likes":2
-    };
+    // const user = {
+    //     "image-url":"https://cdn.britannica.com/26/95926-050-0228E1A6/London-Eye-River-Thames.jpg",
+    //     "userName":"Amy",
+    //     "description":"London Eye is one of most popular tourist attractions in London!",
+    //     "label":["lake","sky wheel"],
+    //     "type":"Landmark",
+    //     "likes":2
+    // };
+
+    const [likes, setLikes] = useState(user.likes)
+
+    const [pressed, setPressed] = useState(false)
+
+    const likesOnClick = () => {
+        if (!pressed) {
+            setLikes(likes + 1)
+            setPressed(!pressed)
+        } else {
+            setLikes(likes - 1)
+            setPressed(!pressed)
+        }
+    }
+    
 
     return (
         <>
-            <Card sx={{ maxWidth: 400 }}>
+            <Card sx={{ maxWidth: 400, marginTop: 5 }}>
                 <CardHeader
                     avatar={
                         <Avatar
@@ -39,14 +55,21 @@ const Post = () => {
                     image={user['image-url']}
                 />
                 <CardContent>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography variant="body3" color="text.primary">
                         {user.description}
+                    </Typography>
+                    <Typography sx={{marginTop: 1}} variant="body2" color="text.secondary">
+                        {"Labels: " + user.label.join(", ")}
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
+                    <IconButton
+                        aria-label="add to favorites"
+                        onClick={likesOnClick}
+                    >
+                        {(!pressed) ? <FavoriteIcon /> : <FavoriteIcon sx={{color: 'red'}}/>}
                     </IconButton>
+                    {likes}
                 </CardActions>
             </Card>
         </>
--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { maxWidth } from '@mui/system';
+import users from '../helpers/local-storage.json';
+import Post from './Post';
 
 const PostList = () => {
+    const [type, setType] = useState('')
+
+    const typeSelection = (event) => {
+        setType(event.target.value);
+    };
+
     return (
         <>
-            <div>this is a post list</div>
+            <Box>
+                <Box sx={{ fontSize: 16, margin: 1 }}>
+                    Sort by:
+                </Box>
+                <Box sx={{ maxWidth: 240, margin: 1 }}>
+                    <FormControl fullWidth>
+                        <InputLabel>Type</InputLabel>
+                        <Select
+                            value={type}
+                            label="Type"
+                            onChange={typeSelection}
+                        >
+                        <MenuItem value={"Food"}>Food</MenuItem>
+                        <MenuItem value={"Views"}>Views</MenuItem>
+                        <MenuItem value={"People"}>People</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+            </Box>
+
+            <Box>
+                {users.map(usr => {
+                    return <Post user={usr}/>
+                })}
+
+            </Box>
         </>
     );
 }
