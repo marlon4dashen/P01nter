e524d9c480638c864d13a08c2e3bd14eb84fcf43
marlon4dashen <alienlql@hotmail.com>
2022-02-27 00:37:39 -0500

    Revert "Merge branch 'main' of https://github.com/manyicheng/Handbook into marlon"
    This reverts commit 463e4b8b59d703eadae9fce6c58aba3fbfa222cf, reversing
    changes made to e575de4c3c80caafb5e509d8c4f4ab3bb443b826.

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -86,8 +86,6 @@ class PostList extends Component {
     render() {
         return (
             <>
-            <Box justifyContent="center" component="span" mt={5}
-                sx={{ display: 'inline-block', mx: '5px', width:'100vw' }}>
             <CreatePost callback={this.handleCreatePost}></CreatePost>
             <Box  sx={ {ml:69 }}>
                 <Box sx={{ fontSize: 16, margin: 1 }}>
@@ -113,12 +111,7 @@ class PostList extends Component {
                 this.state.postarr.filter(post => post.label.includes(this.state.inputLabel)).map(post => (
                     <SinglePost post={post} />))}
 
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-                {this.state.postarr.slice(0).reverse().map(post => {
-                    return <Grid item xs={4}> <SinglePost post={post}/> </Grid>
-                })}
-                </Grid>
-                </Box>
+            </Box>
         </>
         )
     }
